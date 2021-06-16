999ebf25b41aab8da5ba7f9a951104db85368812
alex <kravz2702@gmail.com>
2021-06-16 19:56:59 +0300

    templates
    templates inheritance

--- FlaskMarket/market.py
+++ FlaskMarket/market.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/home')
-def homepage():
+@app.route('/home/')
+def home_page():
     return render_template('home.html')
 
 
