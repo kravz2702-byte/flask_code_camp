a5f9924a33ccf2a782a8c914fd1c30248957cc27
alex <kravz2702@gmail.com>
2021-06-21 22:19:09 +0300

    Validation
    Created simple form and database validations

--- FlaskMarket/market/models.py
+++ FlaskMarket/market/models.py
@@ -8,7 +8,7 @@ class User(db.Model):
     password_hash = db.Column(db.String(length=60), nullable=False)
     budget = db.Column(db.Integer(), nullable=False, default=1000)
     items = db.relationship('Item', backref='owned_user', lazy=True)
-
+    
 
 
 class Item(db.Model):
