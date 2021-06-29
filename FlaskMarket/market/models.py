d7839cdf37784d577aeb850dbaa576aa9974ac20
alex <kravz2702@gmail.com>
2021-06-29 22:35:40 +0300

    user auth part2
    user auth part2

--- FlaskMarket/market/models.py
+++ FlaskMarket/market/models.py
@@ -9,13 +9,20 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), unique=True, nullable=False)
     email_address = db.Column(db.String(length=50), unique=True, nullable=False)
     password_hash = db.Column(db.String(length=60), nullable=False)
     budget = db.Column(db.Integer(), nullable=False, default=1000)
     items = db.relationship('Item', backref='owned_user', lazy=True)
+
+    @property
+    def prettier_budget(self):
+        if len(str(self.budget)) >= 4:
+            return f'{str(self.budget)[:-3]},{str(self.budget)[-3:]}$'
+        else:
+            return f"{self.budget}$"
 
     @property
     def password(self):
