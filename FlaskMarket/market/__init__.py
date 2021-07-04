8b204b8a8b772d439282dcb2db4d62f79341e95d
alex <kravz2702@gmail.com>
2021-07-04 15:57:33 +0300

    login
    login customization

--- FlaskMarket/market/__init__.py
+++ FlaskMarket/market/__init__.py
@@ -9,5 +9,7 @@ app.config['SECRET_KEY'] = 'f16c44bdc32fddbdd1d178cc'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login_page'
+login_manager.login_message_category = "info"
 
 from market import routes
